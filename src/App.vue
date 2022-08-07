bb7277ea2593baca3402d4f470238e887b551367
Herman <smurovnemez@mail.ru>
2022-08-08 02:59:12 +0300

    feat: add navbar design component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
+  <NavBar></NavBar>
+
   <RouterLink to="/" class="button"> Notes </RouterLink> 
   <RouterLink to="/stats" class="button"> Stats </RouterLink>
 
     <RouterView />
 </template>
+
+<script setup>
+  /* 
+    imports
+  */
+
+  import NavBar from '@/components/Layout/NavBar.vue'
+
+</script>
 
 <style>
   @import 'bulma/css/bulma.min.css';
