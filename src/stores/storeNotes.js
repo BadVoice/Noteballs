72382dff9286650fcc3c36e12d71fc68823ef285
Herman <smurovnemez@mail.ru>
2022-08-08 15:28:55 +0300

    feat: state management on the addition of notes

---
+++ src/stores/storeNotes.js
@@ -0,0 +1,33 @@
+import { defineStore } from 'pinia'
+
+export const useStoreNotes = defineStore('storeNotes', {
+  state: () => {
+    return {
+      notes: [
+        {
+          id: 'id1',
+          content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, dicta quis accusantium neque aspernatur magni sit minus atque voluptatem assumenda possimus consequatur corrupti iste asperiores necessitatibus sunt aliquam libero in?'
+      }, 
+      {
+          id: 'id2',
+          content: 'shorter note'
+      }
+      ]
+    }
+  },
+  actions: {
+    addNote(newNoteContent) {
+      if (newNoteContent.trim() == '') {
+            return
+        }
+        let currentDate = new Date().getTime(),
+            id = currentDate.toString()
+
+        let note = {
+            id,
+            content: newNoteContent
+        }
+        this.notes.unshift(note)
+    }
+  }
+})
--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -25,7 +25,7 @@
             </div>
 
             <Note 
-                v-for="note in notes" 
+                v-for="note in storeNotes.notes" 
                 :key='note.id' 
                 :note="note"
                 @deleteClicked="deleteNote"
@@ -40,36 +40,23 @@
     */
     import { ref } from 'vue'
     import  Note  from '@/components/Notes/Note.vue'
+    import { useStoreNotes }  from '@/stores/storeNotes'
+    /* 
+        store
+    */
+
+    const storeNotes = useStoreNotes()
+
     /* 
         notes
     */
 
     const newNote = ref('')
     const newNoteRef = ref(null)
-    const notes = ref([
-        {
-            id: 'id1',
-            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, dicta quis accusantium neque aspernatur magni sit minus atque voluptatem assumenda possimus consequatur corrupti iste asperiores necessitatibus sunt aliquam libero in?'
-        }, 
-        {
-            id: 'id2',
-            content: 'shorter note'
-        }
-    ])
 
     const addNote = () => {
-        if (newNote.value.trim() == '') {
-            return
-        }
-        let currentDate = new Date().getTime(),
-            id = currentDate.toString()
 
-        let note = {
-            id,
-            content: newNote.value
-        }
-
-        notes.value.unshift(note)
+        storeNotes.addNote(newNote.value)
         newNote.value = ''
         newNoteRef.value.focus()
     }
