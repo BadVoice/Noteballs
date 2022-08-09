e1162ee80eb1bc5643427bf6a16769b22dc7f615
Herman <smurovnemez@mail.ru>
2022-08-09 07:48:51 +0300

    refactor: moving AddEditComponent in ViewNote template

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,29 +1,15 @@
 <template>
     <div class="notes">
-        <div class="card has-background-info-light p-4 mb-5">
-            <div class="field">
-                <div class="control">
-                    <textarea 
-                    v-model="newNote"
-                    class="textarea"
-                    placeholder="Add a new note" 
-                    ref="newNoteRef"
-                    />
-                </div>
-            </div>
-
-            <div class="field is-grouped is-grouped-right">
-                    <div class="control">
-                        <button 
-                        @click="addNote"
-                        :disabled="!newNote"
-                        class="button is-info">
-                        Add New Note
-                        </button>
-                    </div>
-                </div>
-            </div>
-
+        <AddEditNote> 
+            <template #buttons>
+                <button 
+                    @click="addNote"
+                    :disabled="!newNote"
+                    class="button is-info">
+                    Add New Note
+                </button>
+            </template>
+        </AddEditNote>
             <Note 
                 v-for="note in storeNotes.notes" 
                 :key='note.id' 
@@ -39,6 +25,7 @@
     */
     import { ref } from 'vue'
     import  Note  from '@/components/Notes/Note.vue'
+    import  AddEditNote  from '@/components/Notes/AddEditNote.vue'
     import { useStoreNotes }  from '@/stores/storeNotes'
     /* 
         store
